613eb9192c87ec1415c7d271c17942de8abbdde3
saseung <seung0122@naver.com>
2023-01-21 20:30:58 +0900

    [PRG] 나머지가 1이 되는 수 찾기 / lv.1 / 4분

---
+++ daon/week2/87389.js
@@ -0,0 +1,9 @@
+//나머지가 1이 되는 수 찾기: https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+function solution(n) {
+  for (let i = 1; i < n; i++) {
+    if (n % i === 1) {
+      return i;
+    }
+  }
+}
