c5c35778c35eefeb2c7d6f2d43fc47e015a82cce
bakhacode <114852081+bakhacode@users.noreply.github.com>
2023-03-05 15:32:55 +0900

    docs: 브렌치 초기화 (#51)
    미숙한 깃 조작으로 엉켜버린 브렌치를 깔끔하게 정리해줬다

--- daon/week2/87389.js
+++
@@ -1,9 +0,0 @@
-//나머지가 1이 되는 수 찾기: https://school.programmers.co.kr/learn/courses/30/lessons/87389
-
-function solution(n) {
-  for (let i = 1; i < n; i++) {
-    if (n % i === 1) {
-      return i;
-    }
-  }
-}
