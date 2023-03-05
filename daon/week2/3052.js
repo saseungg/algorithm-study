eac59f20230e9e1e882f6f363f04678c527b28b7
bakhacode <bakhamitai@naver.com>
2023-03-05 16:43:25 +0900

    docs: main 브렌치 초기화

--- daon/week2/3052.js
+++
@@ -1,9 +0,0 @@
-//나머지: https://www.acmicpc.net/problem/3052
-
-const fs = require("fs");
-const file = process.platform === "linux" ? "dev/stdin" : "./input.txt";
-let input = fs.readFileSync(file).toString().trim().split("\n").map(Number);
-
-let answer = new Set(input.map((el) => el % 42));
-
-console.log(answer.size);
